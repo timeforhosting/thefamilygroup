74f81c2f7bd41e4c3e3df5438c8be715ca6d768e
rudra0 <prudra882@gmail.com>
2020-03-12 01:05:43 +0530

    Updates

---
+++ precache-manifest.00b97150a4a641a2f35f8b7f8c31df43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e9ea5ef326ef11f2f4627369239d69a",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "69ade6cbe94525331751",
+    "url": "/website/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d173339328a6bf14101f",
+    "url": "/website/static/js/2.44ffe768.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/website/static/js/2.44ffe768.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69ade6cbe94525331751",
+    "url": "/website/static/js/main.0e5e0d5a.chunk.js"
+  },
+  {
+    "revision": "13a3a53d375ffd936166",
+    "url": "/website/static/js/runtime-main.4de80ac7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website/static/media/logo.5d5d9eef.svg"
+  }
+]);
