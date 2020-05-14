39558059a91143b77ad690e32cc96c335c865e07
timeforhosting <timeforhosting@gmail.com>
2020-05-15 04:58:28 +0530

    Updates

---
+++ precache-manifest.a543dd8fc4ae87efbb03382ceb1f2ce7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9b1c5ce74aa14225acc36469b438adf",
+    "url": "/thefamilygroup/index.html"
+  },
+  {
+    "revision": "587ed7edd373dbcee90b",
+    "url": "/thefamilygroup/static/css/main.83152a82.chunk.css"
+  },
+  {
+    "revision": "91b0ba8fc8fd53709179",
+    "url": "/thefamilygroup/static/js/2.018fb0cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/thefamilygroup/static/js/2.018fb0cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "587ed7edd373dbcee90b",
+    "url": "/thefamilygroup/static/js/main.c7be8c10.chunk.js"
+  },
+  {
+    "revision": "aa1e10e15424780dd1bb",
+    "url": "/thefamilygroup/static/js/runtime-main.8c3d8af2.js"
+  },
+  {
+    "revision": "e2d2a51c2932205b52f4a5ae1943eb97",
+    "url": "/thefamilygroup/static/media/header-bg.e2d2a51c.jpg"
+  },
+  {
+    "revision": "f40ed5b1e791e9d2479ba8b5e67f2df8",
+    "url": "/thefamilygroup/static/media/map-image.f40ed5b1.png"
+  }
+]);
